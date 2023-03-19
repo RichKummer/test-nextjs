a2db6644583ef7beb2213c6d62f25cdbd695b814
RichKummer <rckummer@gmail.com>
2023-03-19 19:57:21 -0400

    Default + navbar

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <h3>Howdy, this is the nav bar</h3>
+      <p>Just in case, here's some copy</p>
+    </div>
+  );
+}
